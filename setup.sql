6630bb3cfb0ac59a520993b7cf2140076262d44d
s3ansh33p <newfolderlocation@gmail.com>
2021-07-29 21:32:54 +0800

    Production SQL

--- setup.sql
+++ setup.sql
@@ -44,15 +44,15 @@ CREATE TABLE IF NOT EXISTS votes (
     userid bigint(8) NOT NULL
 );
 
--- Insert voting categories
-INSERT INTO categories (catName, descriptor) VALUES ('Best Scene', 'What do you think was the best scene'), ('Best Character', 'Who do you think was the best character');
+-- -- Insert voting categories
+-- INSERT INTO categories (catName, descriptor) VALUES ('Best Scene', 'What do you think was the best scene'), ('Best Character', 'Who do you think was the best character');
 
--- Insert voting nominations
-INSERT INTO nominations (nomName, nomCat) VALUES ('Some Scene I', 1), ('Another Scene II', 1), ('Another Scene III', 1), ('Another Scene IV', 1), ('Another Scene V', 1), ('Another Scene VI', 1), ('Another Scene VII', 1);
-INSERT INTO nominations (nomName, nomCat) VALUES ('Some Actor I', 2), ('Another Actor II', 2), ('Another Actor III', 2), ('Another Actor IV', 2), ('Another Actor V', 2), ('Another Actor VI', 2), ('Another Actor VII', 2);
+-- -- Insert voting nominations
+-- INSERT INTO nominations (nomName, nomCat) VALUES ('Some Scene I', 1), ('Another Scene II', 1), ('Another Scene III', 1), ('Another Scene IV', 1), ('Another Scene V', 1), ('Another Scene VI', 1), ('Another Scene VII', 1);
+-- INSERT INTO nominations (nomName, nomCat) VALUES ('Some Actor I', 2), ('Another Actor II', 2), ('Another Actor III', 2), ('Another Actor IV', 2), ('Another Actor V', 2), ('Another Actor VI', 2), ('Another Actor VII', 2);
 
--- Insert test users
-INSERT INTO users (id, username, discriminator, avatar) VALUES ('1337', 'test', '0001', '1337'), ('1338', 'tester', '0003', '1338'), ('1339', 'bob', '2301', '1339');
+-- -- Insert test users
+-- INSERT INTO users (id, username, discriminator, avatar) VALUES ('1337', 'test', '0001', '1337'), ('1338', 'tester', '0003', '1338'), ('1339', 'bob', '2301', '1339');
 
--- Insert test votes
-INSERT INTO votes (nomCat, nomName1, nomName2, nomName3, nomName4, nomName5, nomName6, nomName7, userid) VALUES ('1', '1', '2', '3', '4', '5', '6', '7', '1337'), ('1', '1', '2', '3', '7', '5', '4', '6', '1338'), ('1', '4', '2', '3', '1', '5', '6', '7', '1339');
+-- -- Insert test votes
+-- INSERT INTO votes (nomCat, nomName1, nomName2, nomName3, nomName4, nomName5, nomName6, nomName7, userid) VALUES ('1', '1', '2', '3', '4', '5', '6', '7', '1337'), ('1', '1', '2', '3', '7', '5', '4', '6', '1338'), ('1', '4', '2', '3', '1', '5', '6', '7', '1339');
